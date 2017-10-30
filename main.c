4f08099fd1ccb4ef0b3524e9f04423906d7043f9
Hassaan Ahmed <hahmed@e1z2r4p41.42.us.org>
2017-10-29 19:30:58 -0700

    new

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: ssumedi <ssumedi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/20 14:39:34 by ssumedi           #+#    #+#             */
-/*   Updated: 2017/10/28 02:56:37 by ssumedi          ###   ########.fr       */
+/*   Updated: 2017/10/29 18:59:05 by hahmed           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -150,13 +150,11 @@ int		**ft_identify(char *str, int count)
 {
 	int		i;
 	int		x;
-	int		y;
 	int		**tetriminos;
 
-	tetriminos = (int**)malloc(sizeof(int) * 3 * count);
 	i = 0;
 	x = 0;
-	y = 0;
+	tetriminos = (int**)malloc(sizeof(int) * 3 * count);
 	while (str[i] != '\0')
 	{
 		if (str[i] == '#')
@@ -195,28 +193,26 @@ char	*ft_grid(int size)
 			j++;
 		}
 		output[i] = '\n';
-		j = 0;
 		i++;
+		j = 0;
 	}
 	output[i] = '\0';
 	return (output);
 }
 
-char	*ft_isavailable(char *str, int a, int b, int c, int count)
+char	*ft_isavailable(char *str, int i, int *array, int count)
 {
-	int		i;
 	char	letter;
 
-	i = 0;
 	letter = 'A' + count;
 	while (str[i])
 	{
-		if (str[i] == '.' && str[i + a] == '.' && str[i + b] == '.' && str[i + c] == '.')
+		if (str[i] == '.' && str[i + array[0]] == '.' && str[i + array[1]] == '.' && str[i + array[2]] == '.')
 		{
 			str[i] = letter;
-			str[i + a] = letter;
-			str[i + b] = letter;
-			str[i + c] = letter;
+			str[i + array[0]] = letter;
+			str[i + array[1]] = letter;
+			str[i + array[2]] = letter;
 			return (str);
 		}
 		i++;
@@ -224,24 +220,49 @@ char	*ft_isavailable(char *str, int a, int b, int c, int count)
 	return (0);
 }
 
-char	*ft_solve(int **str, int count)
+char	*ft_remove(char *str, int i, int *array)
 {
-	int		x;
-	int		size;
-	char	*ptr;
-	
+	int	x;
+
 	x = 0;
-	while (x < count)
-	{	
-		size = 4;
-		ptr = (char*)malloc(sizeof(char) * ((size * (size + 1)) + 1));
-		ptr = ft_grid(size);
-		while (x < count && ptr == ft_isavailable(ptr, str[x][0], str[x][1], str[x][2], x))
+	str[i] = '.';
+	while (array[x])
+	{
+		str[i + array[x]] = '.';
+		x++;
+	}
+	return (str);
+}
+
+int		ft_solve(int **str, int x, int count, int size, char *ptr)
+{
+	unsigned long		i;
+	int		print;
+	
+	if (x > count - 1)
+	{
+		ft_putstr(ptr);
+		return (1);
+	}
+	i = -1;	
+	while (++i < (unsigned long)ft_strlen(ptr))
+	{
+		print = 0;
+		if (ft_isavailable(ptr, i, str[x], x))
 		{
-			x++;
+			print = 1;
+			ptr = ft_isavailable(ptr, i, str[x], x);
+			if (ft_solve(str, x + 1, count, size, ptr))
+				return (1);
+		}
+		if (print)
+		{
+			ptr = ft_remove(ptr, i, str[x]);
 		}
 	}
-	return (ptr);
+	if (i == 0)
+		return (ft_solve(str, 0, count, size + 1, ft_grid(size + 1)));
+	return (0);
 }
 
 int		main(int argc, char **argv)
@@ -249,14 +270,14 @@ int		main(int argc, char **argv)
 	char	*READ;
 	int		CHECK_IF_VALID;
 	int		**IDENTIFY_TETRIMINOS;
-	char	*SOLVE;
+	char	*ptr;
 
+	ptr = ft_grid(4);
 	if (argc != 2)
 		ft_error_usage();
 	READ = ft_read_input(argv[1]);
 	CHECK_IF_VALID = ft_check_valid(READ);
 	IDENTIFY_TETRIMINOS = ft_identify(READ, CHECK_IF_VALID);
-	SOLVE = ft_solve(IDENTIFY_TETRIMINOS, CHECK_IF_VALID);
-	ft_putstr(SOLVE);
+	ft_solve(IDENTIFY_TETRIMINOS, 0, CHECK_IF_VALID, 4, ptr);
 	return (0);
 }
